14b929fb59112bd612b566bb609361a7c281e5be
Joton Sutradhar <53141421+jotonsd@users.noreply.github.com>
2021-01-29 12:39:27 +0600

    Update main.blade.php

--- resources/views/frontendlayout/main.blade.php
+++ resources/views/frontendlayout/main.blade.php
@@ -95,27 +95,27 @@
                               <li class="active"> <a href="index.html">Home</a> </li>
                               <li> <a href="about.html">About</a> </li>
                               <li class="nav-item dropdown megamenu-li">
-                               <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mega Menu 1</a>
+                               <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Projects</a>
                                <div class="dropdown-menu megamenu" aria-labelledby="dropdown01">
                                 <div class="row">
-                                 <div class="col-sm-6 col-lg-3">
+                                 <div class="col-sm-12 col-lg-3">
                                   <h5>Links</h5>
                                   <a class="dropdown-item" href="#">Another action</a>
                                   <a class="dropdown-item" href="#">Something else here</a>
                                   <a class="dropdown-item" href="#">Another action</a>
                                   <a class="dropdown-item" href="#">Something else here</a>
                                  </div>
-                                 <div class="col-sm-6 col-lg-3">
+                                 <div class="col-sm-12 col-lg-3">
                                   <h5>More Links</h5>
                                   <a class="dropdown-item" href="#">Another action</a>
                                   <a class="dropdown-item" href="#">Something else here</a>
                                   <a class="dropdown-item" href="#">Another action</a>
                                  </div>
-                                 <div class="col-sm-6 col-lg-3">
+                                 <div class="col-sm-12 col-lg-3">
                                   <h5>Paragraph</h5>
                                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus in veritatis, facilis eligendi sunt, culpa autem harum porro earum.</p>
                                  </div>
-                                 <div class="col-sm-6 col-lg-3">
+                                 <div class="col-sm-12 col-lg-3">
                                   <h5>Image</h5>
                                   <img src="frontend/images/logo.jpg" alt="..." style="width: 100%;">
                                  </div>
